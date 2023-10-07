b2038043aac1ef68e7c2b5016d90a596317c7e32
Esra <eisra3000ese@gmail.com>
2023-10-07 03:52:13 +0300

    edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,12 @@ int main(void)
 
 	for (x = 0; x < 9; x++)
 	{
-		for (y = x+1 ; y < 10; y++)
+		for (y = x + 1 ; y < 10; y++)
 		{
 			putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
 			if (x == 8 && y == 9)
 				continue;
-	
 			putchar(',');
 			putchar(' ');
 		}
