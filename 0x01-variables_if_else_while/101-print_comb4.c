240ab9e8c329068363766b38415d0cd15a2b8e31
Esra <eisra3000ese@gmail.com>
2023-10-07 04:01:00 +0300

    edit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,16 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 0; x < 9; x++)
+	for (x = 0; x < 8; x++)
 	{
-		for (y = x + 1 ; y < 10; y++)
+		for (y = x + 1 ; y < 9; y++)
 		{
-			for (z = y + 1; z < 11; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
-				if (x == 8 && y == 9 && z ==10)
+				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
