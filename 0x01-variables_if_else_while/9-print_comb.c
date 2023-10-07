3f15d92c3e70f91ce945d078e14f9339ecac0d48
Esra <eisra3000ese@gmail.com>
2023-10-07 03:10:05 +0300

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - prints combinations of single-digit
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
