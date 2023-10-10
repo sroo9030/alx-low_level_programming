c1fd5a4cd28a9f34b5873bcd46eb3c2598c0e909
Esra <eisra3000ese@gmail.com>
2023-10-10 20:28:57 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+}
+
+int main (void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
