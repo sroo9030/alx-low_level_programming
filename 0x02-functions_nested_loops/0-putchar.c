24ae718b848f620298855c381068e4e9bcb11379
Esra <eisra3000ese@gmail.com>
2023-10-10 20:13:49 +0300

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+int main(void)
+{
+	printf ("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02. C - Functions, nested loops
+
+0. _putchar: a program that prints _putchar, followed by a new line.
