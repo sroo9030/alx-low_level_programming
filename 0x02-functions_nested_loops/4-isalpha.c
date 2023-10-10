a1598a08eca73ff19e12ebdd8e185b7eac3414c5
Esra <eisra3000ese@gmail.com>
2023-10-10 22:49:24 +0300

    edit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,11 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks for alphabetic character
- * @c:  The character to be checked
- *
- * Retuen: 1 for alphabetic characters 0 otherwise
- */
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
 int _isalpha(int c)
 {
 	if (isalpha(c))
