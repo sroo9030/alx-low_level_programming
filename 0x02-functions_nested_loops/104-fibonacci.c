0a870a4d83368e540f6766d6fb1e7c23ef3fd515
Esra <eisra3000ese@gmail.com>
2023-10-12 02:05:08 +0300

    edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("%lu, %lu", num1, num2);
 	for (i = 2; i < 98; i++)
 	{
-		if (i != 97)
+		if (i != 98)
 			printf(", ");
 
 		if (num1 + num2 > 10000000000 || num_h2 > 0 || num_h1 > 0)
