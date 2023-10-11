c8567a63c1be5aa934cae080366f5bde0aa280ac
Esra <eisra3000ese@gmail.com>
2023-10-11 03:14:22 +0300

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 
 /**
   * times_table - prints the 9 times table
-  * 
+  *
   * Return: Always 0
   */
 void times_table(void)
@@ -17,15 +17,19 @@ void times_table(void)
 			z = x * y;
 			if (z > 10)
 			{
+				_putchar(44);
+				_putchar(32);
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 			} else
 			{
-			_putchar((z % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+				if (y != 0)
+				{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				}
+				_putchar((z % 10) + '0');
 			}
 		}
 		_putchar('\n');
