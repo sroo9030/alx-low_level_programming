35889e3aca48f64a19312a17ad380617f9db486c
Esra <eisra3000ese@gmail.com>
2023-10-11 19:31:41 +0300

    edit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
   * main - Computes and prints the sum of multiples of 3 or 5
-  * @n: The number that multiply by 3, 5, or 15
   *
   * Return: The sum all the multiples of 3 or 5 below 1024
   */
 int main(void)
 {
-	int n;
+	int n, sum;
 
 	for (n = 0; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0 || n % 15 == 0)
 		{
-			n +=n;
+			sum += n;
 		}
 	}
-	return (n);
+	printf("%d \n", sum);
+	/* putchar(((sum / 10) % 10) + '0');
+	putchar('\n'); */
+	return (0);
 }
