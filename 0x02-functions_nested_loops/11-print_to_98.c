ad951ee6b57683acaa3184401c7e3a0ca6318fa4
Esra <eisra3000ese@gmail.com>
2023-10-11 04:33:52 +0300

    edit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,8 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n >= 98)
+
+	if (n > 98)
 	{
 		for (i = n; i <= 98; i--)
 		{
@@ -19,7 +20,7 @@ void print_to_98(int n)
 			else
 				printf("%d, ", i);
 		}
-	} else 
+	} else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -28,6 +29,9 @@ void print_to_98(int n)
 			else
 				printf("%d, ", i);
 		}
+	} else
+	{
+		printf("%d", n);
 	}
 
 	printf("\n");
