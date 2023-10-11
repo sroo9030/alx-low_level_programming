9d0df1448f3e6767b8f43ca7659a78546a2dce65
Esra <eisra3000ese@gmail.com>
2023-10-12 01:53:15 +0300

    edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,40 @@
 #include <stdio.h>
 
 /**
-  * main - prints the first 50 Fibonacci numbers
+  * main - Prints the first 98 Fibonacci numbers
   *
-  * Return: The summation
+  * Return: Print Fibonacci numbers
   */
 int main(void)
 {
-	unsigned long num1 = 0;
-	unsigned long num2 = 1;
-	unsigned long sum, num1_h1, num1_h2, num2_h1, num2_h2;
-	unsigned long h1, h2; 
+	unsigned long num1 = 1, num2 = 2;
+	unsigned long num_h1 = 0, num_h2 = 0;
+	unsigned long sum, h1, h2;
 	int i;
 
-	num1_h1 = num1 / 10000000000;
-	num1_h2 = num1 % 10000000000;
-	num2_h1 = num2 / 10000000000;
-	num2_h2 = num2 % 10000000000;
-	
+	printf("%lu, %lu, ", num1, num2);
 	for (i = 0; i <= 98; i++)
 	{
-		if (i == 98)
-			printf("%lu\n", sum);
-		else if (i >= 92)
+		if (i != 98 && i != 0)
+			printf(", ");
+
+		if (num1 + num2 > 10000000000 || num_h2 > 0 || num_h1 >0)
 		{
-			h1 = num1_h1 + num2_h1;
-			h2 = num1_h2 + num2_h2;
-			if (h2 > 9999999999)
-			{
-				h1 += 1;
-				h2 %= 10000000000;
-			}
-			printf("%lu%lu", h1, h2);
-			num1_h1 = num2_h1;
-			num1_h2 = num2_h2;
-			num2_h1 = h1;
-			num2_h2 = h2;
+			h1 = (num1 + num2) / 10000000000;
+			h2 = (num1 + num2) % 10000000000;
+			sum = num_h1 + num_h2 + h1;
+			num_h1 = num_h2, num_h2 = sum;
+			num1 = num2, num2 = h2;
+			printf("%lu%010lu", num_h2, num2);
 		}
 		else
 		{
 			sum = num1 + num2;
-			printf("%lu, ", sum);
+			printf("%lu", sum);
 			num1 = num2;
 			num2 = sum;
 		}
 	}
+	printf("\n");
 	return (0);
 }
