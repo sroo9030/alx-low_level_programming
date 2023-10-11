926ca7593eca3f81ee37248badde1e6201ec053c
Esra <eisra3000ese@gmail.com>
2023-10-11 05:31:39 +0300

    edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,29 +13,34 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (x = 0; x <= n ; x++)
-	{
-		for (y = 0; y <= n; y++)
+		for (x = 0; x <= n; x++)
 		{
-			z = x * y;
-			if (z >= 10)
+			_putchar('0');
+
+			for (y = 1; y <= n; y++)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
-			} else
-			{
-				if (y != 0)
+				_putchar(',');
+				_putchar(' ');
+
+				z = x * y;
+
+				if (z <= 99)
+					_putchar(' ');
+				if (z <= 9)
+					_putchar(' ');
+
+				if (z >= 100)
 				{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10)) % 10 + '0');
+				}
+				else if (z <= 99 && z >= 10)
+				{
+					_putchar((z / 10) + '0');
 				}
 				_putchar((z % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
