7a7d59bbd6dd1d0f6f827505f58a8364a02c374f
Esra <eisra3000ese@gmail.com>
2023-10-11 05:10:11 +0300

    n time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_times_table - Prints the n times table
+  * @n: The number of table
+  *
+  * Return: Print table if n between 0 and 15
+  */
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n < 15 || n > 0)
+	{
+		for (x = 0; x < n ; x++)
+	{
+		for (y = 0; y < n; y++)
+		{
+			z = x * y;
+			if (z > 10)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			} else
+			{
+				if (y != 0)
+				{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				}
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	}
+}
