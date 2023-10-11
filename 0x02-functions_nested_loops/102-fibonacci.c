c0538d18364a88994114e11d052a84607ccbbcd5
Esra <eisra3000ese@gmail.com>
2023-10-11 20:57:22 +0300

    edit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,24 +5,29 @@
   *
   * Return: The summation
   */
-int main (void)
+int main(void)
 {
 	long int num1 = 1;
 	long int num2 = 2;
-	long int sum = num1 + num2;
+	long int sum;
 	int i;
 
-	for ( i = 3; i <= 50; i++)
+	for( i = 1; i <= 50; i++)
 	{
 		if (i == 50)
 			printf("%lu \n", sum);
+
+		else if (i < 3)
+			printf("%d, ", i);
+
 		else
 		{
+			sum = num1 + num2;
 			printf("%lu, ", sum);
-		}
 		num1 = num2;
 		num2 = sum;
 		sum = num1 + num2;
+		}
 	}
 	return (0);
 }
