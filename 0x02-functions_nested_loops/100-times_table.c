2f00bf5b617f6f62a29053db1effde3460281dab
Esra <eisra3000ese@gmail.com>
2023-10-11 05:13:24 +0300

    edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (x = 0; x < n ; x++)
+		for (x = 0; x <= n ; x++)
 	{
-		for (y = 0; y < n; y++)
+		for (y = 0; y <= n; y++)
 		{
 			z = x * y;
 			if (z > 10)
