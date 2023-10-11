1ea7d12b0ea8d593d794cb9df5587bd56368a28d
Esra <eisra3000ese@gmail.com>
2023-10-11 03:44:43 +0300

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,25 +12,20 @@ void times_table(void)
 
 	for (x = 0; x < 10; x++)
 	{
+		_putchar('0');
+
 		for (y = 0; y < 10; y++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			z = x * y;
-			if (z > 10)
-			{
-				_putchar(44);
-				_putchar(32);
+			if (z <= 9)
+				_putchar(' ');
+			else
 				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
-			} else
-			{
-				if (y != 0)
-				{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				}
-				_putchar((z % 10) + '0');
-			}
+
+			_putchar((z % 10) + '0');
 		}
 		_putchar('\n');
 	}
