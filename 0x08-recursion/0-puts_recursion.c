7f86a52d795c6642a34939c45a9be6a070c1cc1a
Esra <eisra3000ese@gmail.com>
2023-10-25 21:29:47 +0300

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string
+  * @s: a char type string
+  */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+0x08. C - Recursion
+
+0-puts_recursion.c: a function that prints a string
