68cdcc74820c29f8aa3d4d65ea7558fa99be5339
Esra <eisra3000ese@gmail.com>
2023-10-28 00:15:35 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: an int type argument
+  * @argv: a char type argument
+  *
+  * Return: Always 0
+  */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
