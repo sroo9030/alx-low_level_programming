4ef57ffc2983c8fb5d36ed5a8848d3e31699400e
Esra <eisra3000ese@gmail.com>
2023-10-31 23:34:57 +0300

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,11 +18,12 @@ char *_strdup(char *str)
 		return (NULL);
 
 	n = strlen(str);
-	s = (char *)malloc((n + 1) * sizeof(str));
+	s = (char *)malloc((n + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < n; i++)
+
+	for (i = 0; i <= n; i++)
 	{
 		s[i] = str[i];
 	}
