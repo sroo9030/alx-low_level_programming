7707364e7fa6b673b7cb5dcbb939294e2ab93e55
Esra <eisra3000ese@gmail.com>
2023-11-03 02:38:57 +0300

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s, n1, n2, i, j;
-	char *ptr, *ptr_new;
+	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 0x0C. C - More malloc, free
 
 0-malloc_checked.c: a function that allocates memory using malloc.
+1-string_nconcat.c: a function that concatenates two strings.
