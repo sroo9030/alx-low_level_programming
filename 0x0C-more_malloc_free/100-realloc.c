dfdf7ff1bd4440657ac9d4310668ee854b4339cd
Esra <eisra3000ese@gmail.com>
2023-11-04 02:23:47 +0300

    edit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,9 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size && i < new_size; i++)
-	{
 		p[i] = ((char *)ptr)[i];
-	}
 
 	free(ptr);
 	return (p);
