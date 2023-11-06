61b7b909909e994f564695d8a60f3a7e3349850f
Esra <eisra3000ese@gmail.com>
2023-11-07 00:45:08 +0300

    structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - new type struct dog info
+  * @name: the name
+  * @age: the age
+  * @owner: the owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
