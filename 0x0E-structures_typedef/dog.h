4a6520c424e4bc190fc92a6752f9ee32bbfb1b3e
Esra <eisra3000ese@gmail.com>
2023-11-07 03:48:53 +0300

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
   * @age: the dog age
   * @owner: the name of the owner of the dog
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 /*Functions Prototype*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
