52d1f5f174855c8d3369f87eaee40879b988c3a6
Esra <eisra3000ese@gmail.com>
2023-11-10 00:24:31 +0300

    edit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
