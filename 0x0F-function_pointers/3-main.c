a1edd434e1fe93fcd0a21b50d69c3a53b2409832
Esra <eisra3000ese@gmail.com>
2023-11-10 03:11:39 +0300

    edit main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,20 +18,21 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	if ((b == 0))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
 	op = get_op_func(argv[2]);
 	if (op == NULL)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	printf("%d\n", op(a, b));
 	return (0);
 }
