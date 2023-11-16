bf08b049ffc286bfd885ae0524b2ccaf4c742f37
Esra <eisra3000ese@gmail.com>
2023-11-17 00:24:10 +0300

    edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,29 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
+	list_t *l;
+
+	l = malloc(sizeof(list_t));
+	if (l == NULL)
+	{
+		printf("Error");
+		free(l);
+		return (1);
+	}
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		{
+			l->len = 0;
+			printf("[%u] ", l->len);
+			printf("(nil)\n");
+		}
 		else
+		{
+			printf("[%u] ", h->len);
 			printf("%s\n", h->str);
+		}
 		h = h->next;
 		i++;
 	}
