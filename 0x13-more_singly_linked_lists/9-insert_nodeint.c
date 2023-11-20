16093433cf417e05bd9485df1040b896cd4d43bc
Esra <eisra3000ese@gmail.com>
2023-11-21 00:59:09 +0300

    edit1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,12 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*head == NULL)
-	{
-		new->next = NULL;
-		return (new);
-	}
-	if (idx == 0)
+	if (idx == 0 || *head == NULL)
 	{
 		new->next = *head;
 		*head = new;
