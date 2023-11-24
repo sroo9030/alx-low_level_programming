19a5bdfeffe519bad3e265239edabec05d32c5bc
Esra <eisra3000ese@gmail.com>
2023-11-24 18:29:23 +0300

    get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * get_endianness -  a function that checks the endianness
+  *
+  * Return: 0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) &x;
+
+	return ((int)*c);
+}
