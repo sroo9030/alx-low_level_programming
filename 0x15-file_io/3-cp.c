b16ea7a8fba4757bacd66dac730977b23bef6d2f
Esra <eisra3000ese@gmail.com>
2023-11-28 02:57:35 +0300

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,14 +15,17 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 	f1 = open(av[1], O_RDONLY);
 	if (f1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	f2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 	| S_IRGRP | S_IWGRP | S_IROTH);
 	if (f2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]),
 			close(f1), exit(99);
+
 	while (s1 == 1024)
 	{
 		s1 = read(f1, buf, 1024);
@@ -33,9 +36,12 @@ int main(int ac, char **av)
 		if (s2 < s1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
+
 	if (close(f1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1), exit(100);
+
 	if (close(f2) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2), exit(100);
+
 	return (0);
 }
