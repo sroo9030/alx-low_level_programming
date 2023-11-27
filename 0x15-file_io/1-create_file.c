cc1432364bec8aeb50682c4904fbd044002207ae
Esra <eisra3000ese@gmail.com>
2023-11-28 00:10:21 +0300

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * create_file - a function creates a file
+  * @filename: name of the file to create
+  * @text_content: the content of the file
+  *
+  * Return: 1 if success, -1 otherwise
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int f, w, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	len = (strlen(text_content) + 1);
+	if (text_content == NULL)
+		text_content = "";
+
+	f = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+
+	w = write(f, text_content, len);
+	if (w == -1)
+		return (-1);
+	close(f);
+	return (1);
+}
