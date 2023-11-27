fd6ce8d6225074af2f4edfb137343ea9694098aa
Esra <eisra3000ese@gmail.com>
2023-11-28 00:28:23 +0300

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[len])
-		len++;
-
+	len = (1 + strlen(text_content));
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
