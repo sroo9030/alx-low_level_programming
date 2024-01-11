284090e1e8ecf0689aa32bf55ea5a3b4a9aebb7a
Esra <eisra3000ese@gmail.com>
2024-01-12 01:12:52 +0300

    edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
 /**
+  * print_dlistint - a function that prints all the elements of a list
+  * @h: a structure type point to linked list
+  * Return: the number of nodes
   */
 size_t print_dlistint(const dlistint_t *h)
 {
