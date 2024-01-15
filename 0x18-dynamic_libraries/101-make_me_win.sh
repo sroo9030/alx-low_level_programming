489a74a53c54f683c58ed278b207271bff091151
Esra <eisra3000ese@gmail.com>
2024-01-15 19:35:08 +0300

    edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sroo9030/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/sroo9030/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
