051a97ab66904542bf12dd53fb3a94cd2560d0c9
Esra <eisra3000ese@gmail.com>
2024-01-15 19:26:40 +0300

    win

---
+++ 0x18-dynamic_libraries/win.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+
+int random()
+{
+	static int c = -1;
+
+	c++;
+	if (c == 0 || c == 1)
+		return (8);
+	if (c == 2)
+		return (7);
+	if (c == 3)
+		return (9);
+	if (c == 4)
+		return (23);
+	if (c == 5)
+		return (74);
+	return (c * c % 30000);
+}
