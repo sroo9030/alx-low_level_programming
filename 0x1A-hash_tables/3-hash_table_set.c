bd3faaf6918b4763887c75282a621809e9e994b8
Esra <eisra3000ese@gmail.com>
2024-02-03 01:13:04 +0300

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,11 +9,11 @@
   */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
-	hash_node_t *new_node, *current;
-
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *new_node, *current;
 
 	current = ht->array[index];
 	while (current != NULL)
